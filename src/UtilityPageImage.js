86d81e9b6e736ab0bfa9ce949ef192046e2dc6a7
Matthew Bertolini <matthew-bert@live.ca>
2022-06-26 17:51:36 -0400

    change the UtilityPageImage to generate a random video when checking out the page
    this is temporary and will be removed once videos are created, its just for fun

--- src/UtilityPageImage.js
+++ src/UtilityPageImage.js
@@ -5,6 +5,8 @@ import "./UtilityPageImage.css";
 //clicking on the image will cycle through the throw and land location
 //TODO: add a video icon in the bottom left, when clicked have a popup for a video demonstation of the utility
 
+
+
 export default function UtilityPageImage({
   img_throw,
   img_stand,
@@ -31,7 +33,73 @@ export default function UtilityPageImage({
   const handleToggle4 = () => {
     setActive4(!isActive4);
   };
-
+//START OF RANDOM VIDEO GENERATOR (DELETE WHEN PROPER VIDEOS ARE ADDED)
+  var images = [];
+  var index = 0;
+  images[0] = (
+    <iframe
+      onClick={handleToggle4}
+      src="https://www.youtube.com/embed/hBLl0AMxr7s"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; "
+      allowfullscreen
+      alt={id}
+      className={isActive4 ? "UtilityPageImgGrid" : "UtilityPageImgView"}
+    ></iframe>
+  );
+  images[1] = (
+    <iframe
+      onClick={handleToggle4}
+      src="https://www.youtube.com/embed/FiEzKjycxTw"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; "
+      allowfullscreen
+      alt={id}
+      className={isActive4 ? "UtilityPageImgGrid" : "UtilityPageImgView"}
+    ></iframe>
+  );
+  images[2] = (
+    <iframe
+      onClick={handleToggle4}
+      src="https://www.youtube.com/embed/qFrgdFb0WS0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; "
+      allowfullscreen
+      alt={id}
+      className={isActive4 ? "UtilityPageImgGrid" : "UtilityPageImgView"}
+    ></iframe>
+  );
+  images[3] = (
+    <iframe
+      onClick={handleToggle4}
+      src="https://www.youtube.com/embed/SdH13BXdNqI"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; "
+      allowfullscreen
+      alt={id}
+      className={isActive4 ? "UtilityPageImgGrid" : "UtilityPageImgView"}
+    ></iframe>
+  );
+  images[4] = (
+    <iframe
+      onClick={handleToggle4}
+      src="https://www.youtube.com/embed/N_6XQp08pgM"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; "
+      allowfullscreen
+      alt={id}
+      className={isActive4 ? "UtilityPageImgGrid" : "UtilityPageImgView"}
+    ></iframe>
+  );
+  images[5] = (
+    <iframe
+      onClick={handleToggle4}
+      src="https://www.youtube.com/embed/rP2MDtWu5t0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; "
+      allowfullscreen
+      alt={id}
+      className={isActive4 ? "UtilityPageImgGrid" : "UtilityPageImgView"}
+    ></iframe>
+  );
+  
+  index = Math.floor(Math.random() * images.length);
+  console.log("VIDEO SELECTED: ",index)
+//END OF RANDOM VIDEO GENERATOR
   return (
     <div>
       <div className="parent">
@@ -53,14 +121,7 @@ export default function UtilityPageImage({
           alt={id}
           className={isActive3 ? "UtilityPageImgGrid" : "UtilityPageImgView"}
         />
-        <iframe
-          onClick={handleToggle4}
-          src="https://www.youtube.com/embed/hBLl0AMxr7s"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; "
-          allowfullscreen
-          alt={id}
-          className={isActive4 ? "UtilityPageImgGrid" : "UtilityPageImgView"}
-        ></iframe>
+        {images[index]}
       </div>
     </div>
   );
